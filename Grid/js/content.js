df9292ff80e49c6f2870e622fa62b16071fe6151
Corinne-B <p2081453@mvrht.com>
2017-06-21 19:06:29 +0200

    GenerateImg
    Génération d'image dans chaque balise <section> (Riwad)

--- Grid/js/content.js
+++ Grid/js/content.js
@@ -1,20 +1,22 @@
+
+
 function GenerateContent(raw) {
   console.log(raw);
   var projects = JSON.parse(raw);
   console.log(projects);
-        
-        
-        
-
+  
+  
+      
   var section = document.createElement('section');
   var header = document.createElement('div');
   var title = document.createElement('h2');
   var content = document.createElement('div');
   
   //section.style.backgroundImage = "url('content/img/" + i + ".jpg')";
-  section.style.indexZ = "0"
+  //section.style.backgroundImage = "url('content/hopeandbike/cover.jpg')";
+  //section.style.indexZ = "0"
   section.className += "box container";
-  section.style.backgroundSize = "cover";
+  content.style.backgroundSize = "cover";
   section.style.left = Math.random()*document.getElementById('main').offsetWidth;
   section.style.top = Math.random()*document.getElementById('main').offsetHeight;
   //section.style.position = "absolute";
@@ -42,8 +44,6 @@ function GenerateContent(raw) {
       .removeClass("opac")
       .addClass("move-cursor");
   });
-  //var description = document.createElement('');
-  //content.content;
   
   $(section).on("click", function(event) {
     var container = event.target;
@@ -53,32 +53,54 @@ function GenerateContent(raw) {
       $(".box").addClass('hidden');
       $(container).removeClass('hidden');
       TweenMax.to('.hidden', 0.3, {opacity:0});
+      
+      //TweenMax.staggerTo('.active', 0.3, {left:300, top:1000}, 3);
     } else {
       $(".box").removeClass('hidden');
       TweenMax.to('.box', 0.3, {opacity:1}); 
     } 
   });
-  
-  //description.innerHTML += content.description;
+
   header.className += "header";
   content.className += "content";
   
-  
+
   document.getElementsByTagName('body')[0].appendChild(section);
   (section).appendChild(header);
   (header).appendChild(title);
   (section).appendChild(content);
-  
-  //    var arr = ["Hope & Bike", "Figurines imprimées en 3D", "Imprimante prusa I3", "Onewheel", "TreasureBox", "T-shirt Stromtrooper"];
-  //    title.innerHTML = arr[i];
-  //title.innerHTML += content.title;
-  //    aside.style.backgroundImage = "url('content/img/" + i + ".jpg')";
-  //    aside.style.backgroundSize = "cover";
-  //    console.log(i);
+
+  //aside.style.backgroundSize = "cover";
   title.innerHTML += projects.title;
-  content.innerHTML += projects.content;
+  
+  for(var i = 0;i<projects.content.length;i++){
+    var project = projects.content[i];
+    //console.log(project);
+    content.innerHTML += project.html;
+    var element = content.lastChild;
+    console.log(element);
+    }
+    
+          
+     
+      
+    
 
 }
+
+var foldernames = ["figurines3d", "hopeandbike", "imprimanteprusai3", "onewheel", "treasurebox", "tshirtstormtrooper"];
+
+function GenerateImg(i) {
+  for (var i=0;i<foldernames.length;i++){
+      document.getElementsByTagName('section')[i].style.backgroundImage = "url('/content/"+ foldernames[i]+"/cover.jpg')";
+      console.log(i);
+      console.log('une image est la');
+  }
+}
+    
+
+
+
 
 ///////////////////////////////////LANCEMENT DE LA GENERATION DES PROJETS//////////////////////////////////////////////     
 
@@ -90,17 +112,22 @@ $( function() {
       
   for (var i=0;i<folders.length;i++){
     var req = new XMLHttpRequest();
-    req.open('GET', 'content/'+ folders[i]+'/textContent.json')      
+    req.open('GET', 'content/'+ folders[i]+'/textContent.json');
     req.onreadystatechange = function(event) {
       // XMLHttpRequest.DONE === 4
       if (this.readyState === XMLHttpRequest.DONE) {
         if (this.status === 200) {
           GenerateContent(this.responseText);
+          GenerateImg(i);
+        //}
+          
         } else {
-          console.log("Status de la réponse: %d (%s)", this.status, this.statusText);10
+          console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
         }
       }
     };
     req.send();
   }
 })
+
+
